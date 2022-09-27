6c29abf34907a2850a44deadad331870b393e7ea
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-09-27 22:03:21 +0530

    NLogProg

---
+++ NlogDemo/NlogDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLogs;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter first number");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Plese enter second number");
+int second = Convert.ToInt32(Console.ReadLine());
+Addition.Sum(first, second);
